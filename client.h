978fd0775638595a690db3cc02dcfa6da82e8138
Joshua Moyers <josh@jmoyers.org>
2014-05-22 23:56:05 -0700

    Start sha1 support, start factoring a client out * Unsure how I want to arrange clients connecting to server * Send and receive API from a particular client * Nodes http server api provides request, response objects ...

---
+++ client.h
@@ -0,0 +1,10 @@
+class Peer
+{
+  int m_sock;
+  struct sockaddr m_addr;
+
+  Peer() : 
+    m_addr(),
+    m_sock
+  {}
+}
--- debug.sh
+++ debug.sh
@@ -1,1 +1,1 @@
-clang++ -g -o build/tcp -std=c++11 -stdlib=libc++ -DDEBUG tcp.cpp && ./build/tcp
+clang++ -g -o build/tcp -std=c++11 -stdlib=libc++ -DDEBUG -Ivendor/sha1 tcp.cpp vendor/sha1/libsha.a && ./build/tcp
